8332055b330e82dd3ec0e6bb01b7af1006515a7b
Birdy Bird <edixon@student.neumont.edu>
2023-01-18 16:01:56 -0700

    Debugging
    Rearranged where some methods are called to give priority to obstacle avoidance.

--- Assets/Scripts/Autonomous/AutonomousAgent.cs
+++ Assets/Scripts/Autonomous/AutonomousAgent.cs
@@ -20,33 +20,35 @@ public class AutonomousAgent : Agent
             Debug.DrawLine(this.transform.position, obj.transform.position, Color.red);
         }
 
-        if (objs.Length > 0)
-        {
-            agentMovement.applyForce(Steering.Seek(this, objs[0]) * data.seek_weight);
-            agentMovement.applyForce(Steering.Flee(this, objs[0]) * data.flee_weight);
-        }
-        else
-        {
-            if (agentMovement.acc.sqrMagnitude <= agentMovement.max_force * 0.1f)
-            {
-                agentMovement.applyForce(Steering.Wander(this));
-            }
-        }
+		if (obstacleAvoidance.isObstancleBlocking())
+		{
+			Vector3 direction = obstacleAvoidance.getOpenDirection();
+			agentMovement.applyForce(Steering.Steer(this, direction) * data.obstacle_weight);
+		}
 
-        if (flockView != null) objs = flockView.getGameObjects();
-        if (objs.Length > 0)
-        {
-            agentMovement.applyForce(Steering.Flock(this, objs) * data.flock_weight);
-            agentMovement.applyForce(Steering.FlockNear(this, objs, data.flock_radius) * data.separate_weight);
-            agentMovement.applyForce(Steering.FlockAlign(this, objs) * data.align_weight);
-        }
+		if (objs.Length > 0)
+		{
+			agentMovement.applyForce(Steering.Seek(this, objs[0]) * data.seek_weight);
+			agentMovement.applyForce(Steering.Flee(this, objs[0]) * data.flee_weight);
+		}
+		else
+		{
+			if (agentMovement.acc.sqrMagnitude <= agentMovement.max_force * 0.1f)
+			{
+				agentMovement.applyForce(Steering.Wander(this));
+			}
+		}
 
-        if (obstacleAvoidance.isObstancleBlocking())
-        {
-            Vector3 direction = obstacleAvoidance.getOpenDirection();
-            agentMovement.applyForce(Steering.Steer(this, direction) * data.obstacle_weight);
-        }
-
+		if (flockView != null) objs = flockView.getGameObjects();
+		if (objs.Length > 0)
+		{
+			Vector3 flock_force = Steering.Flock(this, objs) * data.flock_weight;
+			Debug.DrawRay(gameObject.transform.position, flock_force, Color.yellow);
+			agentMovement.applyForce(flock_force);
+			agentMovement.applyForce(Steering.FlockNear(this, objs, data.flock_radius) * data.separate_weight);
+			agentMovement.applyForce(Steering.FlockAlign(this, objs) * data.align_weight);
+		}
+		
         transform.position = Utilities.WrapWorld(transform.position, new Vector3(-50, -50, -50), new Vector3(50, 50, 50));
     }
 }
